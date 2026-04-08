b95b3a1d723995f80636ef0fe25441460c846934
iaraIA <128831253+iaraIA@users.noreply.github.com>
2026-04-08 14:48:09 -0300

    Update urls.py

--- GestionViajes/urls.py
+++ GestionViajes/urls.py
@@ -1,10 +1,8 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('cargarViaje/', views.cargarViaje, name='CargarViaje'),
-    path('verViaje/', views.verViaje, name='VerViaje'),
-    path('misViajes/', views.misViajes , name='misViajes'),
-]   
-    
+    path('verViaje/', views.verViaje, name='verViaje'),
+    path('cargarViaje/', views.cargarViaje, name='cargarViaje'),
+    path('misViajes/', views.misViajes, name='misViajes'),
+]
