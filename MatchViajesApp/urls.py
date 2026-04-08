babadfbb91020186b86103e1c0c41029c191a0e3
iaraIA <128831253+iaraIA@users.noreply.github.com>
2026-04-08 14:34:11 -0300

    Update urls.py

--- MatchViajesApp/urls.py
+++ MatchViajesApp/urls.py
@@ -1,3 +1,16 @@
-from django.urls import path
+from .views import home
+from django.contrib import admin
+from django.urls import path, include
 
-urlpatterns = []
+urlpatterns = [
+    path('', home, name='home'),  # SOLO una raíz
+
+    path('admin/', admin.site.urls),
+
+    # apps
+    path('GestionViajes/', include('GestionViajes.urls')),
+    path('auth/', include('Authentication.urls')),
+
+    # opcional (si realmente lo usás)
+    path('app/', include('MatchViajesApp.urls')),
+]
