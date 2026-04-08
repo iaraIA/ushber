31810984feed631eb6f7447798d0915969328b88
iaraIA <128831253+iaraIA@users.noreply.github.com>
2026-04-08 13:44:20 -0300

    Update urls.py

--- MatchViajes/urls.py
+++ MatchViajes/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('', include('MatchViajesApp.urls')),
     path('GestionViajes/', include('GestionViajes.urls')),
